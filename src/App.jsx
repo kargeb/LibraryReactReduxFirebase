948ee7343cfacad5ee1ccbd04fb98de42d1d5ecc
Karol Gebarowski <karol.gebarowski@gmail.com>
2020-02-03 10:07:13 +0100

    add fetch method and error message as html by dangerouslySetInnerHTML :), BEFORE ROUTING implementation

--- src/App.jsx
+++ src/App.jsx
@@ -3,51 +3,39 @@ import AddBook from "./AddBook";
 import Inventory from "./Inventory";
 import Cart from "./Cart";
 import "./App.css";
-import { Row, Col } from "react-bootstrap";
+import { Row, Col, Alert } from "react-bootstrap";
+import { Route, BrowserRouter } from "@reach/router";
 
 class App extends Component {
   constructor() {
     super();
     this.state = {
       books: [],
-      orders: []
+      orders: [],
+      err: ""
     };
   }
 
   componentDidMount() {
-    this.setState({
-      books: [
-        {
-          title: "Straz! Straz!",
-          author: "Terry Pratchett",
-          publicationDate: "1989",
-          genre: "Fantastyka",
-          audience: "Młodzież"
-        },
-        {
-          title: "Dwie wieze",
-          author: "J. R. R. Tolkien",
-          publicationDate: "1954",
-          genre: "Fantastyka",
-          audience: "Młodzież"
-        },
-        {
-          title: "Komnata Tajemnic",
-          author: "J. K. Rowling",
-          publicationDate: "1998",
-          genre: "Fantastyka",
-          audience: "Młodzież"
-        },
-        {
-          title: "Good Omens",
-          author: "Terry Pratchett",
-          publicationDate: "1990",
-          genre: "Fantastyka",
-          audience: "Młodzież"
-        }
-      ]
-    });
+    this.getBooksFromNet();
   }
+
+  getBooksFromNet = () => {
+    const url = "http://clockworkjava.pl/boooks.php";
+
+    fetch(url)
+      .then(response => {
+        return response.json();
+      })
+      .then(fetchedBooks => {
+        this.setState({ books: [...fetchedBooks] });
+      })
+      .catch(err => {
+        this.setState({
+          err: `Something wrong with downolading book from ${url} <br> ${err}`
+        });
+      });
+  };
 
   addBook = newBook => {
     this.setState(prevState => ({ books: [...prevState.books, newBook] }));
@@ -67,16 +55,34 @@ class App extends Component {
     });
   };
 
+  insertErrorAsHtml() {
+    return (
+      <div
+        dangerouslySetInnerHTML={{
+          __html: `${this.state.err}`
+        }}
+      />
+    );
+  }
+
   render() {
     return (
       <React.StrictMode>
         <div id="created-by-react">
           <Row className="justify-content-md-center bg-primary text-white">
             <h1>React Bookstore</h1>
+            {/* {this.MyComponent()} */}
           </Row>
           <Row>
             <Col sm={8} className="text-center">
-              <Inventory books={this.state.books} addToCart={this.addToCart} />
+              {this.state.err ? (
+                <Alert variant="danger">{this.insertErrorAsHtml()}</Alert>
+              ) : (
+                <Inventory
+                  books={this.state.books}
+                  addToCart={this.addToCart}
+                />
+              )}
             </Col>
             <Col sm={4} className="text-center">
               <AddBook addBook={this.addBook} />
