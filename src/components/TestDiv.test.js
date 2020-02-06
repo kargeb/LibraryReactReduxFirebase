a698282dde3869b1c5bf26a634116c7ba0806371
Karol Gebarowski <karol.gebarowski@gmail.com>
2020-02-06 14:49:06 +0100

    add __mock__ in order to jest work properly with css files

--- src/components/TestDiv.test.js
+++ src/components/TestDiv.test.js
@@ -1,6 +1,6 @@
 import React from "react";
 import ReactDOM from "react-dom";
-import TestDiv from "./TestDiv";
+import { Book } from "./Book";
 
 import { configure, shallow } from "enzyme";
 import Adapter from "enzyme-adapter-react-16";
@@ -8,9 +8,18 @@ import Adapter from "enzyme-adapter-react-16";
 configure({ adapter: new Adapter() });
 
 describe("Enzyme test", () => {
-  it("TestDev render with proper text", () => {
-    const wrapper = shallow(<TestDiv />);
-    console.log(wrapper.debug());
-    expect(wrapper.find("div").text()).toBe("TEST");
+  it('Book render with Button text "Wybierz"', () => {
+    const wrapper = shallow(
+      <Book
+        title="Tytuł"
+        author="autor"
+        publicationDate="1888"
+        audience="Dzieci"
+        genre="Horror"
+        addToCart={() => {}}
+      />
+    );
+    // console.log(wrapper.debug());
+    expect(wrapper.find("Button").text()).toBe("Wybierz");
   });
 });
